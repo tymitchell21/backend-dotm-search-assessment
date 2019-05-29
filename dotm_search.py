b5a3add8cdb53b6b0414fc8b6b5e2228789ef97e
Tyler <tymitchell2100@gmail.com>
2019-05-29 14:32:29 -0400

    refactor session, erases xml changes

--- dotm_search.py
+++ dotm_search.py
@@ -8,50 +8,53 @@ within the 'word/document.xml' section of a MSWord .dotm file.
 __author__ = "Tyler Ward"
 
 from zipfile import ZipFile
-from xml.etree.cElementTree import XML
-from xml.etree import ElementTree
 import os
 import argparse
 import time
 
 def decode(fileName):
+    """reads document.xml from dotm zip file"""
+
     with ZipFile(fileName, 'r') as zip:
         document_xml = zip.read('word/document.xml', pwd=None)
-        tree = XML(document_xml)
-        xmlstr = ElementTree.tostring(tree).decode()
-        return xmlstr
+        return document_xml.decode()
 
-def find_money(dotm_str, path, text):
+def find_text(dotm_str, path, text):
+    """searches through dotm string to find a substring"""
+
     text_location = dotm_str.find(text)
     if text_location != -1:
-        substring = dotm_str[text_location-40:text_location+41]
+        substring = dotm_str[text_location-40:text_location+40+len(text)]
         print('Match found in file ' + path)
         print('...' + substring + '...\n')
-        return 1
-    return 0
+        return True
+    return False
 
 def directory_loop(directory, text):
+    """loops through directories to find text in dotm files"""
     count_searched = 0
     count_matched = 0
     for root, dirs, files in os.walk(directory):
         for filename in files:
             if filename[-4:] == 'dotm':
                 count_searched += 1
-                count_matched += find_money(decode('./dotm_files/' + filename), directory + filename, text)
-    print('Total dotm files searched: ' + str(count_searched))
-    print('Total dotm files matched: ' + str(count_matched))
+                full_path = os.path.join(directory, filename)
+                if find_text(decode(full_path), full_path, text): 
+                    count_matched += 1
+    print(f'Total dotm files searched: {count_searched}')
+    print(f'Total dotm files matched: {count_matched}')
 
 def main():
     """Add your code here"""
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--dir', help='enter a directory')
     parser.add_argument('text')
     args = parser.parse_args()
-    print(args)
     
     if args.dir:
-        print('Searching directory %s for text "$" ...\n\n' % args.dir)
+        print(f'Searching directory {args.dir} for text "$" ...\n\n')
         directory_loop(args.dir, args.text)
     else:
         print('Searching current directory for text "$" ...\n\n')
